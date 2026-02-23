84488619e76d997bb36a8bbec2ef58273b1f844c
Edson Flores <edsonmflores@gmail.com>
2026-02-23 18:02:41 +0100

    Frontend: Añadimos ApiHelper

---
+++ Ofimedic.Frontend/Ofimedic.Frontend/ApiHelper.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Net
+Imports System.Threading.Tasks
+Imports Newtonsoft.Json
+
+Public Module ApiHelper
+    Private client As New WebClient()
+
+    Public Function GetAlbumsAsync(filtro As String) As Task(Of String)
+        Try
+            Dim url = "https://localhost:44300/api/albums"
+            If Not String.IsNullOrEmpty(filtro) Then
+                url &= "?title=" & Uri.EscapeDataString(filtro)
+            End If
+            Return Task.Run(Function() client.DownloadString(url))
+        Catch ex As Exception
+            Throw New Exception("Error al obtener álbumes: " & ex.Message)
+        End Try
+    End Function
+
+    Public Function GetPhotosAsync(albumId As Integer) As Task(Of String)
+        Try
+            Dim url = $"https://localhost:44300/api/photos?albumId={albumId}"
+            Return Task.Run(Function() client.DownloadString(url))
+        Catch ex As Exception
+            Throw New Exception("Error al obtener fotos: " & ex.Message)
+        End Try
+    End Function
+
+    Public Function SyncDataAsync() As Task(Of String)
+        Try
+            Dim url = "https://localhost:44300/api/sync"
+            Return Task.Run(Function() client.UploadString(url, "POST", ""))
+        Catch ex As Exception
+            Throw New Exception("Error al sincronizar: " & ex.Message)
+        End Try
+    End Function
+End Module
---
+++ Ofimedic.Frontend/Ofimedic.Frontend/My Project/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+' asociada a un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+<Assembly: AssemblyTitle("Ofimedic.Frontend")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Ofimedic.Frontend")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
+<Assembly: Guid("3786c783-2834-4df8-91e4-9fe17d33fabe")>
+
+' La información de versión de un ensamblado consta de los siguientes cuatro valores:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
